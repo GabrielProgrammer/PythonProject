996ada3e8b8229f52b07d05403bb25686be3816b
Gabriel <Gabrielsilvaplay16@gmail.com>
2019-06-21 11:29:26 -0300

    Uploading test version

---
+++ teste.py
@@ -0,0 +1,34 @@
+arq = open('sales.csv', 'r')
+dicio_dev = {}
+quant = 0
+i = 0
+for linha in arq:
+    dev = linha.split(',')
+    valor[]
+        if dev not in dicio_dev:
+            quant += 1
+            dicio_dev[dev[14]] = quant
+        elif dev  in dicio_dev:
+            dicio_dev[dev[14]] 
+            quant += 1
+            dicio_dev[dev[14]] = quant
+
+
+
+
+
+
+    
+    for e in dicio_dev:
+        if dev not in dicio_dev:
+            quant += 1
+            dicio_dev[dev[14]] = quant
+        elif dev  in dicio_dev:
+            dicio_dev[dev[14]] 
+            quant += 1
+            dicio_dev[dev[14]] = quant
+
+        
+    
+arq.close()
+print(dicio_dev)
